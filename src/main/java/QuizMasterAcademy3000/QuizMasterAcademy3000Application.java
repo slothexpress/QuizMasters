d99ad57ae33934ec571e600fcc004f8e344c07ec
slothexpress <sami890905@gmail.com>
2019-11-20 10:53:36 +0100

    Uploading project with test print Hello World

---
+++ src/main/java/QuizMasterAcademy3000/QuizMasterAcademy3000Application.java
@@ -0,0 +1,16 @@
+package QuizMasterAcademy3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMasterAcademy3000Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(QuizMasterAcademy3000Application.class, args);
+
+		System.out.println("HELLO WORLD");
+	}
+
+}
